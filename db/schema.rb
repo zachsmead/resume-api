11520c201ccf25e96dc9be85d0521898c7e284b5
Ken Lu <kmlu1993@gmail.com>
2017-05-08 21:29:38 -0700

    added student id to experiences and education

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509025410) do
+ActiveRecord::Schema.define(version: 20170509042614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170509025410) do
     t.text     "details"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "student_id"
   end
 
   create_table "experiences", force: :cascade do |t|
@@ -43,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170509025410) do
     t.text     "details"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "student_id"
   end
 
   create_table "skills", force: :cascade do |t|
